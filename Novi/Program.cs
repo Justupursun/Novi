8eb50c5ec2ba308818ae8facf47b8e07d9a4308f
Justupursun <martijnterstappen@hotmail.com>
2020-08-26 00:03:14 +0200

    Setting up MVC

--- Novi/Program.cs
+++ Novi/Program.cs
@@ -20,7 +20,7 @@ namespace Novi
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>(); 
                 });
     }
 }
--- Novi/Startup.cs
+++ Novi/Startup.cs
@@ -16,6 +16,9 @@ namespace Novi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // register services here through dependency injection
+            services.AddControllersWithViews();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +28,18 @@ namespace Novi
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles(); // looks in wwwroot for static files
+ //           app.UseDefaultFiles();
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                // Map incomming requests with an action controller
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
